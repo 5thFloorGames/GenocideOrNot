a72dc15c6d64e616a61150810f4162502b8212a2
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2018-06-09 20:20:38 +0300

    moving Art to Resources

---
+++ Gornot/Assets/CreateSteps.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSteps : MonoBehaviour {
+
+	private GameObject step;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
