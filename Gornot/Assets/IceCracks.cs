c2fc0d42f0686df7aafe7709e43dbdfc185b790b
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2018-06-10 18:00:29 +0300

    puddle

---
+++ Gornot/Assets/IceCracks.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCracks : MonoBehaviour {
+
+	public Material[] materials;
+	private MeshRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<MeshRenderer>();
+		StartCoroutine(Crack());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Crack(){
+		int i = 0;
+		while(i < materials.Length){
+			rend.material = materials[i];
+			yield return new WaitForSeconds(3);
+		}
+	}
+}
